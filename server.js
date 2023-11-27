57401c01df5bdd511a2ebf5993af0db2f2629962
shaheerkhan12 <shaheer.khan@paysky.io>
2023-11-27 17:04:28 +0500

    changes commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+app.use(cors());
+let broadcaster;
+const port = 5000;
+
+const http = require("http");
+const server = http.createServer(app);
+
+const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+    }
+});
+app.use(express.static(__dirname + "/public"));
+var whitelist = ['http://localhost:4200'];
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  }else{
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+io.sockets.on("error", e => console.log(e));
+io.sockets.on("connection", socket => {
+  socket.on("broadcaster", () => {
+    broadcaster = socket.id;
+    socket.broadcast.emit("broadcaster");
+  });
+  socket.on("watcher", () => {
+    socket.to(broadcaster).emit("watcher", socket.id);
+  });
+  socket.on("offer", (id, message) => {
+    socket.to(id).emit("offer", socket.id, message);
+  });
+  socket.on("answer", (id, message) => {
+    socket.to(id).emit("answer", socket.id, message);
+  });
+  socket.on("candidate", (id, message) => {
+    socket.to(id).emit("candidate", socket.id, message);
+  });
+  socket.on("disconnect", () => {
+    socket.to(broadcaster).emit("disconnectPeer", socket.id);
+  });
+});
+server.listen(port,cors(corsOptionsDelegate), () => console.log(`Server is running on port ${port}`));
